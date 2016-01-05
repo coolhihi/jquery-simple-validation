f4d70a9ffc11b38f8bcf097b64c576b672f60438
COoL <andrewguan@qq.com>
2016-01-05 13:47:00 +0800

    ie bug fixed

--- demo/jquery.simple.validation.js
+++ demo/jquery.simple.validation.js
@@ -1,3 +1,7 @@
+/**
+ * simpleValidation - By Closely Coded
+ * ie bug fixed - By COoL
+ */
 (function($, window, undefined) {
 	var methods = {
 		init : function() {},
@@ -33,14 +37,14 @@
 			return regex.test(text);
 		},
 		isDate : function( dateStr ) {
-			var datePat = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+			var datePat = /^(\d{4})(\/|-)(\d{2})(\/|-)(\d{2})$/;
 			var matchArray = dateStr.match(datePat);
 			if( !datePat.test(dateStr) )
 				return false;
 
-			month = matchArray[1];
-			day = matchArray[3];
-			year = matchArray[5];
+			year = matchArray[1];
+			month = matchArray[3];
+			day = matchArray[5];
 			if( month < 1 || month > 12 )
 				return false;
 			if( day < 1 || day > 31 )
@@ -54,18 +58,18 @@
 					return false;
 			}
 			return true; // date is valid
-		},
+		}
 	};
 
 	$.fn.validate = function( opts ) {
 		var options = $.extend({
-			'short_error_message' : false,
+			'short_error_message' : false
 		}, opts || {});
 		var form = this;
 
-		return $(form.selector).submit(function(ev) {
+		//return $(form.selector).submit(function(ev) {
 			var warning = '';
-			$(form.selector).find('input, select, textarea').each(function() {
+			$(form).find('input, select, textarea').each(function() {
 
 				if( $(this).is(':visible') ) {
 					var data_rules = $(this).data('rules');
@@ -77,93 +81,93 @@
 								var type = rule[1];
 								var value = $(this).val();
 
-							if( options.short_error_message == false ) {
-								label = 'The ' + label + ' field';
-							}
+							//if( options.short_error_message == false ) {
+								//label = 'The ' + label + ' field';
+							//}
 
 							if( type == 'required' && value == '' ) {
-								warning += label + ' is required.\n';
+								warning += label + ' 不允许为空.\n';
 							}
 
 							else if( type == 'valid_email' && !methods.isEmail(value) ) {
-								warning += label + ' must contain a valid email address.\n';
+								warning += label + ' 必须是一个有效的邮箱地址.\n';
 							}
 
 							else if( type == 'valid_url' && value != '' && !methods.isUrl(value) ) {
-								warning += label + ' must contain a valid URL.\n';
+								warning += label + ' 必须是一个有效的网址.\n';
 							}
 
 							else if( type == 'valid_ip' && value != '' && !methods.isIp(value) ) {
-								warning += label + ' must contain a valid IP.\n';
+								warning += label + ' 必须是一个有效的IP.\n';
 							}
 
 							else if( type.substr(0, 10) == 'min_length' && value != '' ) {
 								temp = type.split('+');
-								length = temp[1];
-								if( value.length < length ) {
+								t_length = temp[1];
+								if( value.length < t_length ) {
 									suffix = '';
-									if( length > 1 )
+									if( t_length > 1 )
 										suffix = 's';
-									warning += label + ' must be at least ' + length + ' character' + suffix + ' in length.\n';
+									warning += label + ' 不允许少于 ' + t_length + ' 个字符或 '+Math.floor(t_length/2)+' 个中文.\n';
 								}
 							}
 
 							else if( type.substr(0, 10) == 'max_length' && value != '' ) {
 								temp = type.split('+');
-								length = temp[1];
-								if( value.length > length ) {
+								t_length = temp[1];
+								if( value.length > t_length ) {
 									suffix = '';
-									if( length > 1 )
+									if( t_length > 1 )
 										suffix = 's';
-									warning += label + ' can not exceed ' + length + ' character' + suffix + ' in length.\n';
+									warning += label + ' 不允许多于 ' + t_length + ' 个字符或 '+Math.floor(t_length/2)+' 个中文.\n';
 								}
 							}
 
 							else if( type.substr(0, 12) == 'exact_length' && value != '' ) {
 								temp = type.split('+');
-								length = temp[1];
-								if( value.length != length ) {
+								t_length = temp[1];
+								if( value.length != t_length ) {
 									suffix = '';
-									if( length > 1 )
+									if( t_length > 1 )
 										suffix = 's';
-									warning += label + ' must be exactly ' + length + ' character' + suffix + ' in length.\n';
+									warning += label + ' 必须是 ' + t_length + ' 个字符或 '+Math.floor(t_length/2)+' 个中文.\n';
 								}
 							}
 
 							else if( type.substr(0, 3) == 'min' && value != '' && methods.isInteger(value) ) {
 								temp = type.split('+');
-								limit = temp[1];
-								if( value < limit ) {
-									warning += label + ' must be greater than ' + limit + '.\n';
+								t_limit = temp[1];
+								if( value < t_limit ) {
+									warning += label + ' 不允许小于 ' + t_limit + '.\n';
 								}
 							}
 
 							else if( type.substr(0, 3) == 'max' && value != '' && methods.isInteger(value) ) {
 								temp = type.split('+');
-								limit = temp[1];
-								if( value > limit ) {
-									warning += label + ' must be less than ' + limit + '.\n';
+								t_limit = temp[1];
+								if( value > t_limit ) {
+									warning += label + ' 不允许大于 ' + t_limit + '.\n';
 								}
 							}
 
 							else if( type == 'alpha' && !methods.isAlpha(value) ) {
-								warning += label + ' may only contain alphabetical characters.\n';
+								warning += label + ' 只允许输入英文字符.\n';
 							}
 
 							else if( type == 'alpha_numeric' && !methods.isAlphaNum(value) ) {
-								warning += label + ' may only contain alpha-numeric characters.\n';
+								warning += label + ' 只允许输入英文字符或数字.\n';
 							}
 
 							else if( type == 'numeric' && value != '' && parseFloat(value).toString() != value ) {
-								warning += label + ' must contain only numbers.\n';
+								warning += label + ' 必须是一个数.\n';
 							}
 
 							else if( type == 'is_numeric' && !methods.isNumeric(value) ) {
-								warning += label + ' must contain only numeric characters.\n';
+								warning += label + ' 只允许输入数字.\n';
 							}
 
 							else if( type == 'integer' && value != '' && !methods.isInteger(value) ) {
-								warning += label + ' must contain an integer.\n';
+								warning += label + ' 必须是一个整数.\n';
 							}
 
 							else if( type.substr(0, 7) == 'matches' && value != '' ) {
@@ -171,19 +175,19 @@
 								match_id = temp[1];
 								match_label = temp[2];
 								match_value = $('#'+match_id).val();
-								if( options.short_error_message == false )
-									match_label = 'the ' + label + ' field';
+								//if( options.short_error_message == false )
+								//	match_label = 'the ' + label + ' field';
 								if( value != match_value ) {
-									warning += label + ' does not match with ' + match_label + '.\n';
+									warning += label + ' 必须与 ' + match_label + ' 一致.\n';
 								}
 							}
 
 							else if( type == 'valid_phone' && value != '' && !methods.isPhone(value) ) {
-								warning += label + ' must contain a valid phone number.\n';
+								warning += label + ' 必须是有效的号码.\n';
 							}
 
 							else if( type == 'valid_date' && value != '' && !methods.isDate(value) ) {
-								warning += label + ' must contain a valid date.\n';
+								warning += label + ' 必须是有效的日期.\n';
 							}
 
 							else if( type.substr(0, 11) == 'valid_check' ) {
@@ -200,7 +204,7 @@
 									}
 								});
 								if( flag == 0 )
-									warning += 'Please choose one of ' + label_ori + ' options.\n';
+									warning += '请选择 ' + label_ori + ' .\n';
 							}
 
 							else if( type.substr(0, 11) == 'multi_check' ) {
@@ -214,7 +218,7 @@
 									}
 								});
 								if( flag < match_unit )
-									warning += 'Please choose (min) ' + match_unit + ' of ' + label_ori + ' options.\n';
+									warning += '请选择最少 ' + match_unit + ' 个 ' + label_ori + ' .\n';
 							}
 
 						} //end for
@@ -223,11 +227,11 @@
 			});
 
 			if( warning != '' ) {
-				alert(warning);
-				return false;
+				//alert(warning);
+				return  warning;
 			} else {
 				return true;
 			}
-		});
+		//});
 	};
 })(jQuery, window);
